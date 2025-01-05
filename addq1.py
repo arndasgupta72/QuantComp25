36e648ef7399faf73cd644586317128adc59b0d7
arndasgupta72 <81262907+arndasgupta72@users.noreply.github.com>
2025-01-05 16:00:12 -0700

    Add files via upload

---
+++ addq1.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+from qiskit_aer import  AerSimulator
+from qiskit import transpile
+from qiskit import assemble
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+
+from qiskit.visualization import plot_histogram
+
+
+q = QuantumRegister(5,'q')
+c = ClassicalRegister(2,'c')
+circuit = QuantumCircuit(q,c)
+circuit.x(q[0])
+circuit.x(q[1])
+circuit.x(q[2])
+circuit.cx(q[0],q[3])
+circuit.cx(q[1],q[3])
+circuit.cx(q[2],q[3])
+circuit.ccx(q[0],q[1],q[4])
+circuit.ccx(q[0],q[2],q[4])
+circuit.ccx(q[1],q[2],q[4])
+circuit.measure(q[3],c[0])
+circuit.measure(q[4],c[1])
+
+circuit.draw("mpl")
+plt.show()
+
+aersim=AerSimulator()
+shots=100000
+t_circ=transpile(circuit,aersim)
+qobj = assemble(t_circ, shots=shots)
+result_ideal=aersim.run(qobj).result()
+counts_ideal=result_ideal.get_counts(0)
+
+print(t_circ)
+
+plot_histogram(counts_ideal)
+plt.show()
+
+
